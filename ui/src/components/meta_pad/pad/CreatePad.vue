0da286af30f2e333470ab9e1e8f0ab0feb07bc5f
Matt Gabrenya <matt@buildyourweb.app>
2022-11-07 23:17:30 -0700

    fix: create pad on enter key

--- ui/src/components/meta_pad/pad/CreatePad.vue
+++ ui/src/components/meta_pad/pad/CreatePad.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="m-4 flex flex-col justify-start items-center">
+  <div class="m-4 flex flex-col justify-start items-center" @keydown.enter="createPad()">
     <div class="text-2xl mb-4">Create New Pad</div>
 
     <div class="mb-4">
